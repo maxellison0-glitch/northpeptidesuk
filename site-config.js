5ec3a0073d1f1f3f1fa4e3aaa5be88f758bfd7bf
maxellison0-glitch <maxellison0@gmail.com>
2026-07-06 22:50:07 +0100

    Configure GA4 measurement ID

--- site-config.js
+++ site-config.js
@@ -4,7 +4,7 @@
   window.NPUK_GOOGLE_CONFIG = {
     // Paste one of these public IDs when Google is ready.
     // GA4 example: G-XXXXXXXXXX
-    ga4MeasurementId: '',
+    ga4MeasurementId: 'G-SLBR6QZ84H',
     // GTM example: GTM-XXXXXXX. If this is set, it takes priority over GA4 direct.
     gtmContainerId: '',
   };
